240a234f49b78e94ff10368a6702b9a2c6125a7c
Aissa BOUGUERN <6617326+aissa-bouguern@users.noreply.github.com>
2020-02-12 07:48:25 -0500

    Add key prop

--- src/HomePage.js
+++ src/HomePage.js
@@ -6,7 +6,7 @@ export default function() {
   // authors.find((author) => author.slug === post.author)
   return posts.map(post => {
     return (
-      <li>
+      <li key={post.slug}>
         <Link to={`/blog/${post.slug}`}>{post.title}</Link>
         <span style={{ margin: "0 5px" }}>By</span>
         <Link to={`/user/${post.author}`}>
